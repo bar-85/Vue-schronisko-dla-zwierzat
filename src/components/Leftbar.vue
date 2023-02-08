fb50350c22bfa8e6cb87cbdfe04e8bc99d992608
bar-85 <bajwolskabarbara@gmail.com>
2023-02-08 21:32:24 +0100

    Refresh code

--- src/components/Leftbar.vue
+++ src/components/Leftbar.vue
@@ -1,5 +1,5 @@
 <template>
-	<aside :class="{'open':open}">
+	<aside :class="{ open: open }">
 		<div class="logo">
 			<img src="../assets/logo2.png" alt="logo" @click="ToggleMenu" />
 		</div>
@@ -11,17 +11,17 @@
 
 		<h3>Menu</h3>
 		<div class="menu-bottom">
-			<router-link class="button" to="/" >
+			<router-link class="button" to="/">
 				<span class="material-icons icons">cottage</span>
-				<span class="text" @click="ToggleMenu" >Strona główna</span>
+				<span class="text" @click="ToggleMenu">Strona główna</span>
 			</router-link>
-			<router-link class="button" to="/pets" >
+			<router-link class="button" to="/pets">
 				<span class="material-icons icons">pets</span>
-				<span class="text" @click="ToggleMenu" >Nasze zwierzęta</span>
+				<span class="text" @click="ToggleMenu">Nasze zwierzęta</span>
 			</router-link>
-			<router-link class="button" to="/contact" >
+			<router-link class="button" to="/contact">
 				<span class="material-icons icons">call</span>
-				<span class="text" @click="ToggleMenu" >Kontakt</span>
+				<span class="text" @click="ToggleMenu">Kontakt</span>
 			</router-link>
 		</div>
 	</aside>
