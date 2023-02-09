f125fe239f0ee37b0c17abf7ff527a88b620daa8
bar-85 <bajwolskabarbara@gmail.com>
2023-02-09 01:41:40 +0100

    Add Contact

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,6 +1,33 @@
 <template>
-  <main class="home-page">
-    <h1>Kontakt</h1>
-    <p>To jest kontakt</p>
-  </main>
+	<main class="home-page">
+		<div class="box">
+			<h1>Kontakt</h1>
+			<p>+48 123 456 78</p>
+		</div>
+	</main>
 </template>
+
+<style lang="scss" scoped>
+.home-page {
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+
+	.box {
+		padding: 2rem;
+		border: 1px solid var(--blue);
+		border-radius: 10px;
+		box-shadow: 0 2px 8px rgba(0, 0, 0, 0.25);
+
+    h1 {
+      text-transform: uppercase;
+      margin: 1rem;
+    }
+
+    p {
+      text-align: center;
+    }
+	}
+}
+</style>
