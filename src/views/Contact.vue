16711cd2441a998965806e6271a19a3ab930b961
bar-85 <bajwolskabarbara@gmail.com>
2023-02-09 01:58:58 +0100

    Add @media

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -20,14 +20,25 @@
 		border-radius: 10px;
 		box-shadow: 0 2px 8px rgba(0, 0, 0, 0.25);
 
-    h1 {
-      text-transform: uppercase;
-      margin: 1rem;
-    }
+		h1 {
+			text-transform: uppercase;
+			margin: 1rem;
+		}
 
-    p {
-      text-align: center;
-    }
+		p {
+			text-align: center;
+		}
+	}
+
+	@media (max-width: 978px) {
+	.home-page {
+	
+
+		h1 {
+			font-size: 1.1rem;
+		}
+
+	}
 	}
 }
 </style>
